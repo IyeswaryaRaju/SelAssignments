ae9d11b639a8e5fa4ecfe6ff0dbb597429b4ec7d
IyeswaryaRaju <50798840+IyeswaryaRaju@users.noreply.github.com>
2022-09-17 19:31:53 +0600

    Add files via upload

---
+++ AppleWatch.java
@@ -0,0 +1,21 @@
+package week3.day1;
+
+public class AppleWatch extends SmartWatch {
+
+	public void setAlarm() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void getHeartRate() {
+	}
+
+	@Override
+	public void getHealth() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	}
+
+}
